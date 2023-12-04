f4c528006ca9d8798ff114b0c91f4b73ebfd121f
SHADOW <kenkaneki0957@gmail.com>
2023-12-04 20:24:24 +0530

    remove DSA

---
+++ C++/area_of_circle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main()
+{
+
+    float radius, area;
+    std::cout << "Enter the radius of circle : ";
+
+    std::cin >> radius;
+
+    area = 3.14 * radius * radius;
+
+    std::cout << "Area of circle with radius "
+
+              << radius << " is " << area;
+
+}
